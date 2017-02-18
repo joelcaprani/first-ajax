776a81e7d2e6187913f244c9b9e22f1470d08bb4
Joel Caprani <joelcaprani@Joels-MacBook-Air.local>
2017-02-18 13:05:48 -0500

    did step 3

--- ajax.js
+++ ajax.js
@@ -11,10 +11,21 @@ $(document).ready(function () {
 
       $.ajax({
         url: 'http://first-ajax-api.herokuapp.com',
-        method: 'GET',
-        // data: _____,
-        // dataType: _____
+        method: 'GET'
+
         });
+
+
+    });
+    $('#second').on('click', function(){
+
+    $.ajax({
+      url: 'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET'
+  }).done(function (responseData) {
+      console.log(responseData);
+      $('#step3456').append(responseData);
+  });
 
     });
 });
